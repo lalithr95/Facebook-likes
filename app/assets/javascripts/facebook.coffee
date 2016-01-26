49fec531815137bc9eb9faf191d1fff0210c1bcf
Lalith <lalithr95@gmail.com>
2016-01-27 01:17:29 +0530

    added likes count chart

--- app/assets/javascripts/facebook.coffee
+++ app/assets/javascripts/facebook.coffee
@@ -1,3 +1,40 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+jQuery ->
+	$.ajax
+		url: "/likes",
+		success: (data) ->
+			console.log(data)
+			data = data.data
+			chart = $('#facebook-graph').highcharts
+				chart: 
+					marginBottom: 120
+					reflow: false
+					marginLeft: 50
+					marginRight: 20
+					style:
+						postion: 'absolute'
+				credits:
+					enabled: false
+				title:
+					text: 'Facebook likes'
+				xAxis:
+					type: 'datetime'
+				yAxis:
+					type:
+						text: null
+				plotOptions:
+					series:
+						marker:
+							enabled: false
+							status:
+								hover:
+									enabled: true
+									radius: 3
+				series: [
+					name: 'Facebook Likes'
+					data: data
+				]
+
+
--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,4 +1,5 @@
 require 'koala'
+require 'Date'
 class FacebookController < ApplicationController
 	# Loads basic functionality
 	def index
@@ -15,13 +16,39 @@ class FacebookController < ApplicationController
 	def likes
 		if session[:access_token]
 			@graph = Koala::Facebook::API.new(session[:access_token])
+			result = Array.new
+			data = @graph.get_connection("me", "likes", {limit: 100})
+			data.each do |like|
+				datetime = DateTime.iso8601(like['created_time'])
+				year = datetime.year
+				month = datetime.month
+				day = datetime.day
+				result << "Date.UTC(#{year},#{month},#{day})"
+			end
 			render json: {
-				data: @graph.get_connection("me", "likes", {limit: 100}).next_page
+				data: reduce_obj(result)
 			}
 		else
 			render json: {
 				status: false
 			}
 		end
+	end
+
+	private
+	def reduce_obj result
+		state = Hash.new
+		result.each do |elem|
+			if state[elem]
+				state[elem] += 1
+			else
+				state[elem] = 1
+			end
+		end
+		result = Array.new
+		state.each do |k, v|
+			result << [k, v]
+		end
+		result
 	end
 end
